68b9f6125d5e9b6ec0a3d8a0866690c07f7550cf
Anmol Arora <a53arora@uwaterloo.ca>
2018-05-03 12:32:59 -0400

    Added more docstrings, and code for sha256

--- blockchain.py
+++ blockchain.py
@@ -7,21 +7,19 @@ from block import Block # Not actually using it
 
 import datetime
 import json
-import hashlib
+import hashlib as hasher
 
 class Blockchain:
     def __init__(self):
+        """ Initialize the blockchain """
         self.chain = []
         self.current_transactions = []
         #Creating the genesis block
-        self.add_genesis_block(proof=100, previous_hash=1)
+        self.chain.append(self._create_block(proof=100, previous_hash=1, index=0))
 
-    def add_genesis_block(self, proof, previous_hash):
-        self.chain.append({
-            'index'
-        })
-    
+
     def _create_block(self, proof, previous_hash, index=None):
+        """ Creates and returns a new block using the current transactions """
         if index:
             index = len(self.chain)+1 
         return {
@@ -32,17 +30,14 @@ class Blockchain:
             'previous_hash': previous_hash
         }
 
+
     def add_block(self):
         """Add a new block to blockchain """
         index = len(self.chain)+1
-        block = {
-            'index': index.
-            'timestamp': datetime.datetime.now(),
-            'transactions': self.current_transactions,
-            'proof': proof
-            'previous_hash': self.chain[-1].hash()
-        }
+        block = self._create_block(proof, self.chain[-1].hash())
         self.chain.append(block)
+        self.current_transactions[:] = []   #Empty the list of current transactions once they are added to the block
+
 
     def add_transaction(self, sender, recipient, amount):
         """ Add transaction to be mined in the next block """
@@ -53,12 +48,17 @@ class Blockchain:
             'amount': amount
         })
 
+
     @staticmethod
     def hash(block):
-        pass
+        """ Returns SHA 256 hash of the passed block (JSON Object) """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hasher.sha256(block_string).hexdigest()
+
     
     @property
     def get_last_block(self):
+        """ Gets the last block added to the blockchain """
         return self.chain[-1]
 
 
